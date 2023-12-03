cfb700103a25189c20151123366d2fa7dc0c4816
João Costa <124079686+CostinhaJ@users.noreply.github.com>
2023-12-03 22:22:34 +0000

    java Server/Client KickStarted
    Server/Client methods development

--- Projeto2023SnakeBase/src/remote/RemoteBoard.java
+++ Projeto2023SnakeBase/src/remote/RemoteBoard.java
@@ -49,6 +49,16 @@ public class RemoteBoard extends Board{
 	@Override
 	public void handleKeyPress(int keyCode) {
 		//TODO
+		switch(keyCode){
+		case KeyEvent.VK_DOWN:
+			//return DOWN;
+		case KeyEvent.VK_UP:
+			//return UP;
+		case KeyEvent.VK_LEFT:
+			//return LEFT;
+		case KeyEvent.VK_RIGHT:
+			//return RIGHT;
+		}
 	}
 
 	@Override
